abc326f1558820893012ed4e965a161c00be4a47
akaDRJ <36530505+akaDRJ@users.noreply.github.com>
2025-08-09 23:41:46 +0800

    Update test.js

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ const defaultSelector = [
     "手动切换", "故障转移", "DIRECT"
 ];
 
-const defaultFallback = ["故障转移"];
+const defaultFallback = [];
 
 const globalProxies = [
     "节点选择", "手动切换", "故障转移", "静态资源", "人工智能", "加密货币", "PayPal", "Telegram", "Microsoft", "Apple", "Google", "YouTube", "Disney", "Netflix", "Spotify", "Twitter(X)", "学术资源", "开发者资源", "游戏平台", "Speedtest", 
